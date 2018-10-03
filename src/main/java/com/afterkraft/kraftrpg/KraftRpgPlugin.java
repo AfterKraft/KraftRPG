a6ed639531475893de8e4eededec85110184ea82
Michael Mansfield <mmansfield721@gmail.com>
2018-10-02 20:31:47 -0400

    Beginning implementation

--- src/main/java/com/afterkraft/kraftrpg/KraftRpgPlugin.java
+++ src/main/java/com/afterkraft/kraftrpg/KraftRpgPlugin.java
@@ -1,11 +1,25 @@
 package com.afterkraft.kraftrpg;
 
 import com.afterkraft.kraftrpg.api.RpgKeys;
-import com.afterkraft.kraftrpg.api.RpgPlugin;
+
+import com.google.inject.Inject;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.slf4j.Logger;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+
 import org.spongepowered.api.data.key.Key;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.GameRegistryEvent;
+import org.spongepowered.api.event.game.state.GameLoadCompleteEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 @Plugin(
         id = "kraftrpg",
@@ -18,8 +32,78 @@ public class KraftRpgPlugin {
 
     @Listener
     public void keyRegister(GameRegistryEvent.Register<Key<?>> event) {
+
         event.register(RpgKeys.RPG_EFFECTS);
+        event.register(RpgKeys.PRIMARY_ROLE);
         event.register(RpgKeys.ADDITIONAL_ROLES);
+        event.register(RpgKeys.SECONDARY_ROLE);
+        event.register(RpgKeys.PARTY);
+        event.register(RpgKeys.BASE_DAMAGE);
+        event.register(RpgKeys.DAMAGE_MODIFIER);
+        event.register(RpgKeys.MANA);
+        event.register(RpgKeys.MAX_MANA);
+        event.register(RpgKeys.SUMMON_DURATION);
+        event.register(RpgKeys.REWARDING_EXPERIENCE);
+    }
+
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private Path configFile;
+
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path configDirectory;
+
+    private ConfigurationNode config;
+
+    @Listener
+    public void preInitialization(GamePreInitializationEvent event) {
+        try {
+            config = configManager.load();
+
+            if (!configFile.toFile().exists()) {
+                config.getNode("KraftRPG Config Placeholder").setValue(true);
+                configManager.save(config);
+            }
+
+
+        } catch (IOException e) {
+            logger.warn("KraftRPG could not successfully load the configuration!");
+        }
+    }
+
+    @Listener
+    public void gameLoaded(GameLoadCompleteEvent event) {
+        
+
+    }
+
+
+    //Plugin Logger initialization
+    private Logger logger;
+
+    @Listener
+    //Load up KraftRPG
+    public void onServerStart(GameStartedServerEvent event) {
+        logger.info("KraftRPG was successfully loaded!");
+
+
+    }
+
+    public Path getConfigDirectory() {
+        return configDirectory;
+    }
+
+
+    public Logger getLogger() {
+
+        return logger;
     }
 
 }
